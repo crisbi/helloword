1ce485bb5df2d022076c5ac9d283eb3f17a989c6
crisbi <ferreiramartinscristina@gmail.com>
2024-10-31 09:23:04 -0300

    Create app01.py

---
+++ app01.py
@@ -0,0 +1,44 @@
+# Criar uma variável
+
+'''
+Esse comentário pode
+ter muitas
+e muitas
+linhas
+'''
+
+"""
+Esse comentário também
+pode ter muitas
+e muitas
+linhas
+"""
+
+
+# Criar uma variável usando 'snake_case'
+cor_da_casa = "azul"
+
+# Criar uma variável usando "camelcase"
+corDacasa = 'amarela'
+
+# Exibe as coisas
+print('n\n\n\')
+      
+
+# Exibe as coisa
+print(cor_da_casa)
+
+
+# First name
+first_name = 'Janine'
+
+# Last name
+last_name = 'Impuha'
+
+
+# Imprime my name
+print (first_name)
+print(last_name)
+
+#imprime os dois nomes
+print(first_name_ last_name)
